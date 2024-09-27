36e4764c0f2a6ac069e9512013da6383364651e5
Kévin Le Perf <codlabtech@gmail.com>
2024-09-27 22:20:22 +0200

    add command to register a result for 2 opponents

--- lorcana-discord/src/main/kotlin/eu/codlab/discord/ScrimRegisterScore.kt
+++ lorcana-discord/src/main/kotlin/eu/codlab/discord/ScrimRegisterScore.kt
@@ -6,9 +6,32 @@ import eu.codlab.discord.utils.LorcanaData
 import korlibs.time.DateTime
 import me.jakejmattson.discordkt.arguments.AnyArg
 import me.jakejmattson.discordkt.arguments.IntegerArg
+import me.jakejmattson.discordkt.commands.SlashCommandEvent
 import me.jakejmattson.discordkt.commands.commands
 
-@Suppress("TooGenericExceptionCaught", "MagicNumber")
+fun scrimRegisterScoreOther() = commands("Scrim", BotPermissions.EVERYONE) {
+    globalSlash("scrimOthers", "Register a scrim result for 2 players") {
+        execute(
+            AnyArg("opponent1", "The first player's discord tag, e.g. @Someone1"),
+            AnyArg("opponent2", "The second player's discord tag, e.g. @Someone2"),
+            IntegerArg("roundWonPlayer1", "Number of rounds won by the first player"),
+            IntegerArg("roundWonPlayer2", "Number of rounds won by the second player"),
+        ) {
+            val player1 = args.first.toDiscord()!!
+            val player2 = args.second.toDiscord()!!
+            val roundWon1 = args.third
+            val roundWon2 = args.fourth
+
+            registerScore(
+                player1,
+                player2,
+                roundWon1,
+                roundWon2
+            )
+        }
+    }
+}
+
 fun scrimRegisterScore() = commands("Scrim", BotPermissions.EVERYONE) {
     globalSlash("scrim", "Register a scrim result") {
         execute(
@@ -16,47 +39,61 @@ fun scrimRegisterScore() = commands("Scrim", BotPermissions.EVERYONE) {
             IntegerArg("roundWon", "Number of rounds you won"),
             IntegerArg("roundLost", "Number of rounds you lost"),
         ) {
-            val scrimTrack = LorcanaData.database.scrimTracker
-
-            val discord = args.first.toDiscord()
+            val discord = args.first.toDiscord()!!
             val roundWon = args.second
             val roundLost = args.third
 
-            val player1 = ScrimPlayerResult(
-                discordId = author.id.value.toLong(),
-                roundWon = roundWon
+            registerScore(
+                author.id.value.toLong(),
+                discord,
+                roundWon,
+                roundLost
             )
+        }
+    }
+}
 
-            val player2 = ScrimPlayerResult(
-                discordId = discord!!,
-                roundWon = roundLost
-            )
+@Suppress("TooGenericExceptionCaught", "MagicNumber")
+private suspend fun SlashCommandEvent<*>.registerScore(
+    player1Id: Long,
+    player2Id: Long,
+    roundWonPlayer1: Int,
+    roundWonPlayer2: Int
+) {
+    val scrimTrack = LorcanaData.database.scrimTracker
 
-            val pair = if (player1.roundWon >= player2.roundWon) {
-                player1 to player2
-            } else {
-                player2 to player1
+    val player1 = ScrimPlayerResult(
+        discordId = player1Id,
+        roundWon = roundWonPlayer1
+    )
+
+    val player2 = ScrimPlayerResult(
+        discordId = player2Id,
+        roundWon = roundWonPlayer2
+    )
+
+    val pair = if (player1.roundWon >= player2.roundWon) {
+        player1 to player2
+    } else {
+        player2 to player1
+    }
+
+    try {
+        scrimTrack.insert(
+            guildOrAuthorFallback.value.toLong(),
+            DateTime.now().unixMillisLong / 1000,
+            player1 = pair.first,
+            player2 = pair.second
+        )
+
+        respondPublic {
+            field("Scrim") {
+                "The result has been set"
             }
-
-            try {
-                scrimTrack.insert(
-                    guildOrAuthorFallback.value.toLong(),
-                    DateTime.now().unixMillisLong / 1000,
-                    player1 = pair.first,
-                    player2 = pair.second
-                )
-
-                respondPublic {
-                    field("Scrim") {
-                        "The result has been set"
-                    }
-                }
-            } catch (err: Throwable) {
-                err.printStackTrace()
-                respondPublic {
-                    field("Error") { "Couldn't load this tournament info" }
-                }
-            }
+        }
+    } catch (ignored: Throwable) {
+        respondPublic {
+            field("Error") { "Couldn't load this tournament info" }
         }
     }
 }
